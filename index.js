b642a994a7ae8dec991b5f88362f714984a40c7b
Michael Martinez <zeroeclipse@live.com>
2021-04-25 13:58:58 -0700

    commit tree 66832f7bcac31af17a89653067a712347e561360

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let array = []
+    for (let i = 0; i < names.length; i++ ) {
+      array.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return array
+  }
+  
+  function countDown(num) {
+    while (num > 0) {
+      console.log(num);
+      num -= 1;
+    }
+    console.log(num);
+  }
